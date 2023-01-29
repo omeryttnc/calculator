c3df835da1f4671a73e4efb1e9c32384556537c8
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2023-01-29 19:00:25 +0000

    first commit

---
+++ app/src/main/java/com/example/mycalculator2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.mycalculator2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+
+import android.widget.Button
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_LONG).show()
+    }
+}
