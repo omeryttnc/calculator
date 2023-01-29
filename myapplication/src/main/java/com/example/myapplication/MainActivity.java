6a4ea39dad72a30777bd0a923bcf34cbbb541e11
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2023-01-29 21:39:19 +0000

    95

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onDigit (View view){
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show();
     }
 }
